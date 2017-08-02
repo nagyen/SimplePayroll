34060f4a7bbd7930b7dca1a579abc587001ba41f
Nagarjuna Yendluri <nag.yendluri09@gmail.com>
2017-08-02 05:48:50 -0400

    added state field to employee

--- core/Domain/Employee.cs
+++ core/Domain/Employee.cs
@@ -13,6 +13,7 @@ namespace core.Domain
         public long Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string State { get; set; }
         public string SSN { get; set; }
         public decimal Insurance { get; set; }
         public int Retirement401k { get; set; }
