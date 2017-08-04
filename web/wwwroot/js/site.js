55b5ce00c8730bdc8da654fbc2dc6ba6e3c30566
nagarjuna yendluri <nagarjuna_yendluri@student.uml.edu>
2017-08-04 09:10:57 -0400

    Added required libraries for web

--- web/wwwroot/js/site.js
+++ web/wwwroot/js/site.js
@@ -1,1 +1,10 @@
 ﻿angular.module("app", []);
+$("#grid-basic").bootgrid();
+$("#date-picker").datepicker({
+    autoclose: true,
+    todayHighlight: true
+});
+
+function bootboxtest() {
+    bootbox.alert("working");
+}
